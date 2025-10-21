2c763bb00358dd4a50ef6e5638286da17ab2f8ce
Ian-Guzman <ianman36@outlook.com>
2025-10-21 01:17:38 -0400

    Update bitops64.h

--- bitops64.h
+++ bitops64.h
@@ -2,9 +2,9 @@
 #define BITOPS64_H
 
 #include <stdint.h>
-#include "bitops.h"  // your original 32-bit functions
+#include "bitops.h"  // original 32-bit functions
 
-// 64-bit wrappers built from your 32-bit bitops
+// 64-bit wrappers built 32-bit bitops
 unsigned long long SetBit64(unsigned long long value, int position);
 unsigned long long ClearBit64(unsigned long long value, int position);
 int GetBit64(unsigned long long value, int position);
